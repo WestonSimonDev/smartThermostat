f068a251d88122cd1b22aec889cddd618ad47e2f
WestonSimonDev <weston@wcloud.dev>
2024-05-31 21:14:01 +0000

    Added user interface

---
+++ interfaces/userinterface/src/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+        <div className={styles.container}></div>
+    );
+  }
+}
+export default App;
